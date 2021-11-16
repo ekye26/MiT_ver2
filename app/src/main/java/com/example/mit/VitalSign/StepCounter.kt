c0f994e4de639a9c90e7ac6a1b2cf41a07e2e353
daye <77138279+ekye26@users.noreply.github.com>
2021-11-16 16:24:42 +0900

    data delete

--- app/src/main/java/com/example/mit/VitalSign/StepCounter.kt
+++ app/src/main/java/com/example/mit/VitalSign/StepCounter.kt
@@ -173,9 +173,9 @@ class StepCounter: AppCompatActivity() {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-        val jdbcURL = "jdbc:postgresql://203.255.56.50:5432/thingsboard"
-        val username = "postgres"
-        val password = "7452"
+        val jdbcURL = ""
+        val username = 
+        val password = 
 
         try {
             val connection = DriverManager.getConnection(jdbcURL, username, password)
